287e08b435ce34f651d9ec4fdfe9610ea6d423c9
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-05-25 18:19:10 +0200

    update files.js

---
+++ files.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+// Reading files
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) console.log(err);
+//   if (data) {
+//     console.log(data.toString());
+//   }
+// });
+
+//Writting files
+// fs.writeFile("./docs/blog1.txt", "Hello, call me SUGIRA", () => {
+//   console.log("Text was written!");
+// });
+// fs.writeFile("./docs/blog2.txt", "Hello, call me NGENDAHAYO", () => {
+//   console.log("Text was written!");
+// });
+
+// Directories
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err, dir) => {
+//     if (err) console.log(err);
+//     console.log(`Directory was created!`);
+//   });
+// } else {
+//   console.log("Directory already exists!, taking time to remove it...");
+//   setTimeout(() => {
+//     fs.rmdir("./assets", (err) => {
+//       if (err) console.log(err);
+
+//       console.log("Directory removed!");
+//     });
+//   }, 3000);
+// }
+
+//Delete files
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
+    if (err) console.log(err);
+    console.log("File deleted succesfully!")
+  });
+  
+}
