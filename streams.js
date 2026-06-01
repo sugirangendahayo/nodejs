ae8118b1d6f4120eff728323bc890527f091b085
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-06-01 18:36:22 +0200

    update streams.js

--- streams.js
+++ streams.js
@@ -7,7 +7,7 @@ const writeStream = fs.createWriteStream("./docs/blog4.txt");
 
 readStream.on("data", (chunk) => {
   console.log("----- NEW CHUNK -----\n");
-  console.log(chunk);
+  console.log(chunk.toString());
   writeStream.write(`\nNEW CHUNK\n`);
   writeStream.write(chunk);
 });
