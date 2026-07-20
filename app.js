f0d4eb871aff4d81c3fe407211c21fc842440021
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-07-20 16:53:57 +0200

    minor changes

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
-app.listen(3000, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
-app.get("/", (req, res)=>{
-  res.render("index")
-})
-app.get("/about", (req, res)=>{
+const blogs = [
+  {
+    id: 1,
+    title: "First Blog",
+    content: "This is the content of the first blog.",
+  },
+  {
+    id: 2,
+    title: "Second Blog",
+    content: "This is the content of the second blog.",
+  },
+];
+app.get("/", (req, res) => {
+  res.render("index", { blogs });
+});
+app.get("/about", (req, res) => {
   res.render("about");
-})
-app.get("/blogs/create", (req, res)=>{
+});
+app.get("/blogs/create", (req, res) => {
   res.render("create");
-})
-app.use((req, res)=>{
+});
+app.use((req, res) => {
   res.status(404).render("404");
-})
+});
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
